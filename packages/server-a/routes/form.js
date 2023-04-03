aadced4614998e219d5bffb8f814ba8944625681
Gaurav Mall <dev.gauravmall@gmail.com>
2023-04-03 14:25:21 +0530

    Update form.js

--- packages/server-a/routes/form.js
+++ packages/server-a/routes/form.js
@@ -6,5 +6,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/../public/form.html'));
 });
+router.post('/', function(req, res, next) {
+  res.redirect('/?formposted=success')
+});
+
 
 module.exports = router;
